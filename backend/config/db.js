02ef7cc79b04c9b60a38596f2c43fd3087abdd2e
Your Name <soumitrasahoo030@gmail.com>
2025-10-12 11:53:12 +0530

    Backend issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://soumitrasahoo030_db_user:RaDPEYI9u3MREgAI@cluster0.ecw0j7f.mongodb.net/food-del"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("DB Connected!");
   } catch (error) {
     console.error("DB Connection Failed:", error);
