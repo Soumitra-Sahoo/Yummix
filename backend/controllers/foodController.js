24e74187c416a99ab5d9ec4e174f8c210044c4f6
Your Name <soumitrasahoo030@gmail.com>
2025-10-12 11:08:13 +0530

    Cloudinary added

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -1,5 +1,5 @@
 import foodModel from "../models/foodModel.js";
-import fs from 'fs'
+import { v2 as cloudinary } from 'cloudinary';
 
 // all food list
 const listFood = async (req, res) => {
@@ -16,14 +16,12 @@ const listFood = async (req, res) => {
 // add food
 const addFood = async (req, res) => {
 
-    let image_filename = `${req.file.filename}`
-
     const food = new foodModel({
         name: req.body.name,
         description: req.body.description,
         price: req.body.price,
         category:req.body.category,
-        image: image_filename,
+        image: req.file.path,
     })
     try {
         await food.save();
@@ -39,7 +37,8 @@ const removeFood = async (req, res) => {
     try {
 
         const food = await foodModel.findById(req.body.id);
-        fs.unlink(`uploads/${food.image}`, () => { })
+        const public_id = food.image.split('/').pop().split('.')[0];
+        await cloudinary.uploader.destroy(public_id);
 
         await foodModel.findByIdAndDelete(req.body.id)
         res.json({ success: true, message: "Food Removed" })
--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,21 +1,32 @@
 import express from 'express';
 import { addFood, listFood, removeFood } from '../controllers/foodController.js';
 import multer from 'multer';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+
 const foodRouter = express.Router();
 
-//Image Storage Engine (Saving Image to uploads folder & rename it)
+// Cloudinary configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
-const storage = multer.diskStorage({
-    destination: 'uploads',
-    filename: (req, file, cb) => {
-        return cb(null,`${Date.now()}${file.originalname}`);
-    }
-})
+// Multer-storage-cloudinary configuration
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'Yummix',
+        format: async (req, file) => 'png', // supports promises as well
+        public_id: (req, file) => `${Date.now()}-${file.originalname}`,
+    },
+});
 
-const upload = multer({ storage: storage})
+const upload = multer({ storage: storage });
 
-foodRouter.get("/list",listFood);
-foodRouter.post("/add",upload.single('image'),addFood);
-foodRouter.post("/remove",removeFood);
+foodRouter.get("/list", listFood);
+foodRouter.post("/add", upload.single('image'), addFood);
+foodRouter.post("/remove", removeFood);
 
 export default foodRouter;
